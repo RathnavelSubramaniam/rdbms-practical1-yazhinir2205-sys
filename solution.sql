e1a68fa9314aa39e240fcd9e4b44d7032b173a66
yazhinir2205-sys <yazhini.r2205@gmail.com>
2026-07-30 09:29:40 +0530

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -1,4 +1,4 @@
 create database college DBBCA;
 use college DBBCA;
 create table departement(departement_id int primary key,departement_name varchar(100),hod varchar(100));
-select*from departement
+select*from departement;
